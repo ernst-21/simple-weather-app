da213a6a7f243779aa8e55de977651ff5fdac814
ernst-21 <riverag8504@gmail.com>
2021-06-24 02:09:15 -0400

    improved css

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
   }, [units]);
 
   const onChange = (checked) => {
-    console.log(`switch to ${checked}`);
     if (checked) {
       setUnits('metric');
     } else {
@@ -46,90 +45,86 @@ function App() {
   const utc = d.getTime() + d.getTimezoneOffset() * 60000;
 
   const onSearch = (value) => {
-    const finalValue = handleSpaces(value);
-    console.log(finalValue);
-    setIsLoading(true);
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${finalValue}&units=${units}&appid=${APIkey}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (data && data.cod === 200) {
-          setTimeData(
-            new Date(utc + (3600000 * data.timezone) / 3600).toLocaleString()
-          );
-          setWeatherData(data);
-          setIconURL(
-            ` http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
-          );
-          setIsLoading(false);
-          console.log(data);
-        } else if (data && data.cod === '404') {
-          console.log(data.message);
-          setError(data.message);
-          setIsLoading(false);
-        } else if (!data) {
-          setError('No network connection');
-          setIsLoading(false);
-        }
-      })
-      .catch((err) => {
-        if (err) {
-          setError('Something went wrong.');
-        }
-      });
+    if (value) {
+      const finalValue = handleSpaces(value);
+      setIsLoading(true);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${finalValue}&units=${units}&appid=${APIkey}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data && data.cod === 200) {
+            setTimeData(
+              new Date(utc + (3600000 * data.timezone) / 3600).toLocaleString()
+            );
+            setWeatherData(data);
+            setIconURL(
+              ` http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
+            );
+            setIsLoading(false);
+          } else if (data && data.cod === '404') {
+            setError(data.message);
+            setIsLoading(false);
+          } else if (!data) {
+            setError('No network connection');
+            setIsLoading(false);
+          }
+        })
+        .catch((err) => {
+          if (err) {
+            setError('Something went wrong.');
+          }
+        });
+    } else if (value === '') {
+      return null;
+    }
   };
 
   const antIcon = <LoadingOutlined style={{ fontSize: 100 }} spin />;
 
   return (
-    <>
+    <div className="main-container">
+      <Search
+        placeholder="input city name"
+        //allowClear
+        onSearch={onSearch}
+        style={{ width: 300 }}
+        enterButton
+      />
       <Switch
-        style={{ float: 'right', marginRight: '3rem' }}
+        style={{ marginTop: '2rem' }}
         onChange={onChange}
         checkedChildren=" °C "
         unCheckedChildren=" °F "
         size="default"
       />
-      <div className="main-container">
-        <Search
-          placeholder="input city name"
-          //allowClear
-          onSearch={onSearch}
-          style={{ width: 400 }}
-        />
-        {isLoading && (
-          <Spin style={{ marginTop: '6rem' }} indicator={antIcon} />
-        )}
-        {weatherData && (
-          <div
-            style={{ marginTop: '2rem', display: 'grid', placeItems: 'center' }}
-          >
-            <div style={{ display: 'grid', placeItems: 'center' }}>
-              <h1>{weatherData.name}</h1>
-              <h3>{timeData}</h3>
-            </div>
-            {iconURL.length > 0 && (
-              <img src={iconURL} alt={weatherData.weather[0].icon} />
-            )}
-            <h3>
-              {Math.round(weatherData.main.temp_max)}° /{' '}
-              {Math.round(weatherData.main.temp_min)}° Feels like:{' '}
-              {weatherData.main.feels_like} {units === 'metric' ? ' °C' : ' °F'}
-            </h3>
-            <h3>Humidity : {weatherData.main.humidity}%</h3>
-            <h3 className="description">
-              {weatherData.weather[0].description}
-            </h3>
+      {isLoading && <Spin style={{ marginTop: '6rem' }} indicator={antIcon} />}
+      {weatherData && (
+        <div
+          style={{ marginTop: '1rem', display: 'grid', placeItems: 'center' }}
+        >
+          <div style={{ display: 'grid', placeItems: 'center' }}>
+            <h1>{weatherData.name}</h1>
+            <h3>{timeData}</h3>
           </div>
-        )}
-        {error && (
-          <h2 style={{ textTransform: 'capitalize', marginTop: '2rem' }}>
-            {error}
-          </h2>
-        )}
-      </div>
-    </>
+          {iconURL.length > 0 && (
+            <img src={iconURL} alt={weatherData.weather[0].icon} />
+          )}
+          <h3>
+            {Math.round(weatherData.main.temp_max)}° /{' '}
+            {Math.round(weatherData.main.temp_min)}° Feels like:{' '}
+            {weatherData.main.feels_like} {units === 'metric' ? ' °C' : ' °F'}
+          </h3>
+          <h3>Humidity : {weatherData.main.humidity}%</h3>
+          <h3 className="description">{weatherData.weather[0].description}</h3>
+        </div>
+      )}
+      {error && (
+        <h2 style={{ textTransform: 'capitalize', marginTop: '4rem'}}>
+          {error}
+        </h2>
+      )}
+    </div>
   );
 }
 
